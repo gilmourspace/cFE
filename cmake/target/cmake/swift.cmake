7bfd41efc288023b8e97f101567e9f9e40d05e98
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2021-11-19 10:31:38 +1000

    Made swift.cmake use a function.

--- cmake/target/cmake/swift.cmake
+++ cmake/target/cmake/swift.cmake
@@ -1,21 +1,21 @@
 cmake_minimum_required (VERSION 3.15.3)
 
-# projectname is the same as the main-executable
-project(CFESWIFT)
-
 #set(SWIFT_ALL_LIBS swiftCore util m swiftSwiftOnoneSupport icui18nswift icuucswift icudataswift stdc++ gcc_s)
 
-set(SWIFT_LIBS swiftCore swiftSwiftOnoneSupport icui18nswift icuucswift icudataswift)
+function(add_swift_libraries)
 
-set(SWIFT_SYSTEM_LIBS util m stdc++ gcc_s)
+    set(SWIFT_LIBS swiftCore swiftSwiftOnoneSupport icui18nswift icuucswift icudataswift)
 
-set(SWIFT_ALL_LIBS ${SWIFT_LIBS} ${SWIFT_SYSTEM_LIBS})
+    set(SWIFT_SYSTEM_LIBS util m stdc++ gcc_s)
 
-set(TARGET_LINK_FLAGS ${TARGET_LINK_FLAGS} "-Wl,-R/usr/lib/swift/linux" "-Wl,-L/usr/lib/swift/linux")
+    set(SWIFT_ALL_LIBS ${SWIFT_LIBS} ${SWIFT_SYSTEM_LIBS})
 
-foreach(SWIFTLIB ${SWIFT_LIBS})
-    add_library(${SWIFTLIB} SHARED IMPORTED)
-    set_target_properties(${SWIFTLIB} PROPERTIES IMPORTED_LOCATION "/usr/lib/swift/linux/lib${SWIFTLIB}.so")
-endforeach()
+    set(TARGET_LINK_FLAGS ${TARGET_LINK_FLAGS} "-Xlinker --no-as-needed" "-Wl,-R/usr/lib/swift/linux" "-Wl,-L/usr/lib/swift/linux")
 
+    foreach(SWIFTLIB ${SWIFT_LIBS})
+        add_library(${SWIFTLIB} SHARED IMPORTED)
+        set_target_properties(${SWIFTLIB} PROPERTIES IMPORTED_LOCATION "/usr/lib/swift/linux/lib${SWIFTLIB}.so")
+    endforeach()
+
+endfunction(add_swift_libraries)
 
